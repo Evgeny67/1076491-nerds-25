17a04e2c6265b742d61197a4b9d67442837bc1b0
Evgeny67 <50173484+Evgeny67@users.noreply.github.com>
2019-06-11 09:32:44 +0300

    Добавлен файл js

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+var link = document.querySelector(".map");
+var popup = document.querySelector(".modal");
+var close = popup.querySelector(".modal-close");
+
+link.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.add("modal-show");
+});
+
+close.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.remove("modal-show");
+});
