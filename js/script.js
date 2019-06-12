c597f47bdd09fa4409b0187171c598ea01be224e
Evgeny67 <50173484+Evgeny67@users.noreply.github.com>
2019-06-12 21:19:13 +0300

    Убраны пустые строки

--- js/script.js
+++ js/script.js
@@ -1,14 +1,11 @@
-
-var link = document.querySelector(".map");
+var link = document.querySelector(".button-send");
 var popup = document.querySelector(".modal");
 var close = popup.querySelector(".modal-close");
-
 link.addEventListener("click", function (evt) {
-    evt.preventDefault();
+    /*evt.preventDefault();*/
     popup.classList.add("modal-show");
 });
-
 close.addEventListener("click", function (evt) {
-    evt.preventDefault();
+    /*evt.preventDefault();*/
     popup.classList.remove("modal-show");
 });
