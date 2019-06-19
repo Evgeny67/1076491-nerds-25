bb006696cbb98055a98f226233bdd4908a10cacf
Evgeny67 <50173484+Evgeny67@users.noreply.github.com>
2019-06-19 09:19:33 +0300

    Убраны не нужные закомментированные строки

--- js/script.js
+++ js/script.js
@@ -2,10 +2,8 @@ var link = document.querySelector(".button-send");
 var popup = document.querySelector(".modal");
 var close = popup.querySelector(".modal-close");
 link.addEventListener("click", function (evt) {
-    /*evt.preventDefault();*/
     popup.classList.add("modal-show");
 });
 close.addEventListener("click", function (evt) {
-    /*evt.preventDefault();*/
     popup.classList.remove("modal-show");
 });
